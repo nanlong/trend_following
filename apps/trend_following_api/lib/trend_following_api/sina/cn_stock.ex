dffd16af5a736c4508ed099c0ee3399c032c9919
Jeff <200006506@qq.com>
2017-09-07 16:56:30 +0800

    Add TrendFollowingApi.Sina.CNStock

---
+++ apps/trend_following_api/lib/trend_following_api/sina/cn_stock.ex
@@ -0,0 +1,138 @@
+defmodule TrendFollowingApi.Sina.CNStock do
+  @moduledoc """
+  TrendFollowingApi.Sina.CNStock.get("list", page: 1)
+  TrendFollowingApi.Sina.CNStock.get("dayk", symbol: "sh600036")
+  TrendFollowingApi.Sina.CNStock.get("mink", symbol: "sh600036", scale: 5)
+  TrendFollowingApi.Sina.CNStock.get("mink", symbol: "sh600036", scale: 60)
+  TrendFollowingApi.Sina.CNStock.get("detail", symbol: "sh600036")
+  """
+  use HTTPotion.Base
+
+  @list_api "http://gu.sina.cn/hq/api/openapi.php/StockV2Service.getNodeList"
+  @k_api "http://money.finance.sina.com.cn/quotes_service/api/json_v2.php/CN_MarketData.getKLineData"
+  @detail_api "http://hq.sinajs.cn"
+
+  def process_url("list", query) do
+    query = [
+      page: Keyword.get(query, :page, 1),
+      num: Keyword.get(query, :num, 20)
+    ]
+
+    process_url(@list_api, query)
+  end
+
+  def process_url("dayk", query) do
+    query = [
+      symbol: Keyword.get(query, :symbol),
+      scale: 240,
+      datalen: 0
+    ]
+    
+    process_url(@k_api, query)
+  end
+
+  @doc """
+  scale: 5 | 15 | 30 | 60
+  """
+  def process_url("mink", query) do
+    query = [
+      symbol: Keyword.get(query, :symbol),
+      scale: Keyword.get(query, :scale, 5),
+      datalen: 0
+    ]
+
+    process_url(@k_api, query)
+  end
+
+  def process_url("detail", query) do
+    rn = (System.system_time() / 1_000_000_000) |> round()
+    list = "#{query[:symbol]},#{query[:symbol]}_i"
+    @detail_api <> "/?rn=#{rn}&list=#{list}"
+  end
+
+  def process_url(url, query) do
+    process_url(url)
+    |> prepend_protocol
+    |> append_query_string([query: query])
+  end
+
+  def process_response_body(body) do
+    :iconv.convert("gbk", "utf-8", body) 
+    |>  IO.iodata_to_binary()
+    |> decode()
+  end
+
+  def decode("var" <> _ = data) do
+    %{"symbol" => symbol} = Regex.named_captures(~r/str_(?<symbol>\w+)=/, data)
+    [[_, d1], [_, d2]] = Regex.scan(~r/"(.*)"/, data)
+    
+    if String.length(d1) <= 0 or String.length(d2) <= 0 do
+      %{}
+    else
+      d1 = String.split(d1, ",") |> List.to_tuple()
+      d2 = String.split(d2, ",") |> List.to_tuple()
+
+      {open, _} = elem(d1, 1) |> Float.parse()
+      {pre_close, _} = elem(d1, 2) |> Float.parse()
+      {price, _} = elem(d1, 3) |> Float.parse()
+      {highest, _} = elem(d1, 4) |> Float.parse()
+      {lowest, _} = elem(d1, 5) |> Float.parse()
+      {volume, _} = elem(d1, 8) |> Integer.parse()
+      {amount, _} = elem(d1, 9) |> Float.parse()
+      # 最近报告的每股净资产
+      {navps, _} = elem(d2, 5) |> Float.parse()
+      # 最近四个季度净利润
+      {eps, _} = elem(d2, 13) |> Float.parse()
+      # 总股本
+      {total_capital, _} = elem(d2, 7) |> Float.parse()
+      # 流通股本
+      {cur_capital, _} = elem(d2, 8) |> Float.parse()
+      # 总市值
+      market_cap = (price * total_capital * 10_000) |> round()
+      # 流通市值
+      cur_market_cap = (price * cur_capital * 10_000) |> round()
+      # 市盈率
+      pe = if eps <= 0, do: 0, else: (market_cap / eps / 100_000_000) |> Float.round(2)
+      # 市净率
+      pb = (price / navps) |> Float.round(2)
+      # 换手率
+      turnover = (volume / cur_capital / 100) |> Float.round(2)
+      # 涨跌额
+      diff = (price - pre_close) |> Float.round(2)
+      # 涨跌幅
+      chg = (diff / pre_close * 100) |> Float.round(2)
+      # 振幅
+      amplitude = ((highest - lowest) / pre_close * 100) |> Float.round(2)
+
+      %{
+        "symbol" => symbol,
+        "name" => elem(d1, 0),
+        "price" => price,
+        "open" => open,
+        "highest" => highest,
+        "lowest" => lowest,
+        "pre_close" => pre_close,
+        "volume" => volume,
+        "amount" => amount,
+        "market_cap" => market_cap,
+        "cur_market_cap" => cur_market_cap,
+        "turnover" => turnover,
+        "pb" => pb,
+        "pe" => pe,
+        "diff" => diff,
+        "chg" => chg,
+        "amplitude" => amplitude,
+        "datetime" => "#{elem(d1, 30)} #{elem(d1, 31)}"
+      }
+    end
+  end
+
+  def decode(data) do
+    {:ok, data} =
+      ~r/(?<={|,)\w+(?=:)/
+      |> Regex.replace(data, "\"\\g{0}\"")
+      |> Poison.decode
+      
+    data
+  end
+end
--- apps/trend_following_api/test/trend_following_api_test.exs
+++ apps/trend_following_api/test/trend_following_api_test.exs
@@ -1,6 +1,72 @@
 defmodule TrendFollowingApiTest do
   use ExUnit.Case
 
+  describe "sina cn stock" do
+    alias TrendFollowingApi.Sina.CNStock
+
+    test "list" do
+      assert %{status_code: 200, body: data} = CNStock.get("list", page: 1)
+      assert is_map(data)
+      assert Map.has_key?(data, "result")
+      data = Map.get(data, "result")
+      assert Map.has_key?(data, "data")
+      data = Map.get(data, "data")
+      assert Map.has_key?(data, "data")
+      [dayk | _] = Map.get(data, "data")
+      assert is_map(dayk)
+      assert Map.has_key?(dayk, "ext")
+      ext = Map.get(dayk, "ext")
+      assert is_map(ext)
+      assert Map.has_key?(ext, "symbol")
+      assert Map.has_key?(ext, "name")
+    end
+
+    test "dayk" do
+      assert %{status_code: 200, body: [dayk | _]} = CNStock.get("dayk", symbol: "sh600036")
+      assert is_map(dayk)
+      assert Map.has_key?(dayk, "day")
+      assert Map.has_key?(dayk, "open")
+      assert Map.has_key?(dayk, "close")
+      assert Map.has_key?(dayk, "high")
+      assert Map.has_key?(dayk, "low")
+      assert Map.has_key?(dayk, "volume")
+    end
+
+    test "mink" do
+      assert %{status_code: 200, body: [mink | _]} = CNStock.get("mink", symbol: "sh600036")
+      assert is_map(mink)
+      assert Map.has_key?(mink, "day")
+      assert Map.has_key?(mink, "open")
+      assert Map.has_key?(mink, "close")
+      assert Map.has_key?(mink, "high")
+      assert Map.has_key?(mink, "low")
+      assert Map.has_key?(mink, "volume")
+    end
+
+    test "detail" do
+      assert %{status_code: 200, body: data} = CNStock.get("detail", symbol: "sh600036")
+      assert is_map(data)
+      assert Map.has_key?(data, "symbol")
+      assert Map.has_key?(data, "name")
+      assert Map.has_key?(data, "price")
+      assert Map.has_key?(data, "open")
+      assert Map.has_key?(data, "highest")
+      assert Map.has_key?(data, "lowest")
+      assert Map.has_key?(data, "pre_close")
+      assert Map.has_key?(data, "volume")
+      assert Map.has_key?(data, "amount")
+      assert Map.has_key?(data, "market_cap")
+      assert Map.has_key?(data, "cur_market_cap")
+      assert Map.has_key?(data, "turnover")
+      assert Map.has_key?(data, "pb")
+      assert Map.has_key?(data, "pe")
+      assert Map.has_key?(data, "diff")
+      assert Map.has_key?(data, "chg")
+      assert Map.has_key?(data, "amplitude")
+      assert Map.has_key?(data, "datetime")
+    end
+  end
+
   describe "sina us stock" do
     alias TrendFollowingApi.Sina.USStock
 
